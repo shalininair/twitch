9ef71e6c707249b82e3f25a88d72510d391c6b98
Shalini Nair <shalininair75@gmail.com>
2015-08-03 20:18:13 +0530

    Eliminates repitition - Solves exercise 3.1

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+   @base_title = 'Twitch'
+  end
  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select 'title', 'Home | Twitch'
+    assert_select 'title', "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select 'title', 'Help | Twitch'
+    assert_select 'title', "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select 'title', 'About | Twitch'
+    assert_select 'title', "About | #{@base_title}"
   end
 
 end
